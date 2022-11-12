4c3292ec36bb5b7763e4af889cde7f6fc3ce0d2d
erfanekrami <erfanak964@gmail.com>
2022-11-12 15:44:17 +0330

    update grid

--- packages/ui/src/svgIcon/svgIcon.tsx
+++ packages/ui/src/svgIcon/svgIcon.tsx
@@ -3,24 +3,24 @@ import { joinClassNames, joinStyles } from "@yakad/lib";
 import styles from "./svgIcon.module.css";
 
 interface SvgIconProps extends React.HTMLAttributes<HTMLElement> {
-  size?: number;
+    size?: number;
 }
 
 function SvgIcon(props: SvgIconProps) {
-  const joinedClassNames = joinClassNames(styles.svg, props.className!);
+    const joinedClassNames = joinClassNames(styles.svg, props.className!);
 
-  const joinedStyles = joinStyles(
-    props.size
-      ? { width: props.size + "rem", height: props.size + "rem" }
-      : { width: "2.4rem", height: "2.4rem" },
-    props.style!
-  );
+    const joinedStyles = joinStyles(
+        props.size
+            ? { width: props.size + "rem", height: props.size + "rem" }
+            : {},
+        props.style!
+    );
 
-  return (
-    <div {...props} className={joinedClassNames} style={joinedStyles}>
-      {props.children}
-    </div>
-  );
+    return (
+        <div {...props} className={joinedClassNames} style={joinedStyles}>
+            {props.children}
+        </div>
+    );
 }
 
 export default SvgIcon;

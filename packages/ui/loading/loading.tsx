05b23bb1df800a5bc435194adff1c347c5f8d51d
erfanekrami <erfanak964@gmail.com>
2022-11-27 12:53:59 +0330

    add loading components

---
+++ packages/ui/loading/loading.tsx
@@ -0,0 +1,24 @@
+import styles from "./loading.module.css";
+import { joinClassNames } from "@yakad/lib";
+
+interface LoadingProps extends React.HTMLAttributes<HTMLButtonElement> {
+    size?: "small" | "medium" | "large";
+    variant?: "scaleOut" | "dots" | "spinner";
+}
+
+function Loading(props: LoadingProps) {
+    const joinedClassNames = joinClassNames(
+        styles.loading,
+        props.size ? styles[props.size] : styles.medium,
+        props.variant ? styles[props.variant] : styles.spinner,
+        props.className!
+    );
+
+    return (
+        <div className={joinedClassNames}>
+            <div></div>
+        </div>
+    );
+}
+
+export default Loading;

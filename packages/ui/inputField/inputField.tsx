a3bc67ff155a6523220bf6dfb8040f7ea2cf9665
erfanekrami <erfanak964@gmail.com>
2022-12-14 15:59:33 +0330

    Add InputFieid components

---
+++ packages/ui/inputField/inputField.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { joinClassNames } from "@yakad/lib";
+import styles from "./inputField.module.css";
+
+interface InputFieldProps extends React.HTMLAttributes<HTMLInputElement> {
+    variant?: "standard" | "outlined" | "filled";
+    type?: string;
+    placeholder?: string;
+    disabled?: boolean;
+}
+
+function InputField(props: InputFieldProps) {
+    const joinedClassNames = joinClassNames(
+        styles.input,
+        props.variant ? styles[props.variant] : styles.standard,
+        props.className!
+    );
+
+    return (
+        <label className={styles.field}>
+            <input
+                disabled={props.disabled}
+                placeholder="&nbsp;"
+                type={props.type}
+                className={joinedClassNames}
+            />
+            <span className={styles.placeholder}>{props.placeholder}</span>
+        </label>
+    );
+}
+
+export default InputField;

bbbdbf9df5ee8e085202c53135451917c8f16799
erfanekrami <erfanak964@gmail.com>
2023-01-11 11:14:53 +0330

    Add Thame components

---
+++ packages/ui/thame/thame.tsx
@@ -0,0 +1,20 @@
+import Reat from "react";
+import { joinClassNames, joinStyles } from "@yakad/lib";
+import styles from "./thame.module.css";
+
+interface ThameProps extends React.HTMLAttributes<HTMLElement> {
+    mode?: "light" | "dark" | "system";
+    color?: "green" | "pink" | "grow" | "blue";
+}
+
+function Thame(props: ThameProps) {
+    const joinedClassNames = joinClassNames(
+        styles.blue,
+        props.mode ? styles[props.mode] : styles.blue,
+        props.color ? styles[props.color] : styles.blue,
+        props.className!
+    );
+    return <div className={joinedClassNames}>{props.children}</div>;
+}
+
+export default Thame;

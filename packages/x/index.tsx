b63e9a66c920ccc62ca313be3215b9896139cd75
al-abd <nexnema@gmail.com>
2024-06-15 10:38:19 +0330

    up x

--- packages/x/index.tsx
+++ packages/x/index.tsx
@@ -2,7 +2,5 @@ import XgetStart from "./XgetStart/XgetStart";
 import Xbackground from "./Xbackground/Xbackground";
 import Xpanel from "./Xpanel/Xpanel";
 import Xtable from "./Xtable/Xtable";
-import XbackButton from "./XbackButton/Xbackbutton";
-import XforwardButton from "./XforwardButton/Xforwardbutton";
 
-export { XgetStart, Xbackground, Xpanel, Xtable, XbackButton, XforwardButton };
+export { XgetStart, Xbackground, Xpanel, Xtable };

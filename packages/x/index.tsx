77e8cadb163ad8bdb30fb3eda89f64c912a6b54f
Martis5 <sjadk757@gmail.com>
2024-06-22 15:44:09 +0330

    Rename Xnav => XmenuList

--- packages/x/index.tsx
+++ packages/x/index.tsx
@@ -2,6 +2,7 @@ import XgetStart from "./XgetStart/XgetStart";
 import Xbackground from "./Xbackground/Xbackground";
 import Xpanel from "./Xpanel/Xpanel";
 import Xtable from "./Xtable/Xtable";
-import XnavigationList from "./XnavigationList/XnavigationList";
+import XmenuList from "./XmenuList/XmenuList";
+import XloginBox from "./XloginBox/XloginBox";
 
-export { XgetStart, Xbackground, Xpanel, Xtable, XnavigationList };
+export { XgetStart, Xbackground, Xpanel, Xtable, XmenuList, XloginBox };

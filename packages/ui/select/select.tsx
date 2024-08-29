955f056c2ebf04560eaaf413c566586eadd508e4
ekiuse <0ekiuse1@gmail.com>
2024-08-29 17:44:45 +0330

    update select

--- packages/ui/select/select.tsx
+++ packages/ui/select/select.tsx
@@ -3,26 +3,28 @@ import { joinClassNames } from "@yakad/lib";
 import styles from "./select.module.css";
 
 export interface SelectProps
-  extends DetailedHTMLProps<
-    SelectHTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement
-  > {
-  variant?: "outlined" | "filled";
-  placeholder?: string;
+    extends DetailedHTMLProps<
+        SelectHTMLAttributes<HTMLSelectElement>,
+        HTMLSelectElement
+    > {
+    variant?: "outlined" | "filled";
+    placeholder?: string;
 }
 export default function Select(props: SelectProps) {
-  const joinedClassNames = joinClassNames(
-    styles.select,
-    props.variant ? styles[props.variant] : styles.outlined,
-    props.className!
-  );
-  return (
-    <div className={styles.div}>
-      <select {...props} className={joinedClassNames}>
-        {props.children}
-      </select>
-      <label className={styles.label}>{props.placeholder}</label>
-      <div className={styles.borderdiv}></div>
-    </div>
-  );
+    const joinedClassNames = joinClassNames(
+        styles.select,
+        props.variant ? styles[props.variant] : styles.outlined,
+        props.placeholder ? styles.havePlaceHolder : "",
+        props.className!
+    );
+    return (
+        <div className={styles.div}>
+            <select {...props} className={joinedClassNames}>
+                {props.children}
+            </select>
+            {props.placeholder ? (
+                <label className={styles.label}>{props.placeholder}</label>
+            ) : null}
+        </div>
+    );
 }

075bd61dc8148876e5d7532a172ec51930f5a017
al-abd <nexnema@gmail.com>
2025-02-06 18:03:16 +0330

    refactor: simplify exports

--- packages/x/src/index.ts
+++ packages/x/src/index.ts
@@ -1,30 +1,11 @@
-// Types
-export type { XbackgroundProps } from "./Xbackground/Xbackground";
-export type { XdataMapProps } from "./XdataMap/XdataMap";
-export type { XgetStartProps } from "./XgetStart/XgetStart";
-export type { XpanelProps } from "./Xpanel/Xpanel";
-export type { XmenuListProps } from "./XmenuList/XmenuList";
-export type { XloginBoxProps } from "./XloginBox/XloginBox";
-export type { XTableProps } from "./Xtable/Xtable";
-export type { XTheadProps } from "./Xtable/Xthead";
-export type { XTbodyProps } from "./Xtable/Xtbody";
-export type { XTfootProps } from "./Xtable/Xtfoot";
-export {
-    type XTrProps,
-    type XThProps,
-    type XTdProps,
-    type XTfProps,
-} from "./Xtable/Xtrhdf";
-
-// Components
-export { default as Xbackground } from "./Xbackground/Xbackground";
-export { default as XdataMap } from "./XdataMap/XdataMap";
-export { default as XgetStart } from "./XgetStart/XgetStart";
-export { default as Xpanel } from "./Xpanel/Xpanel";
-export { default as XmenuList } from "./XmenuList/XmenuList";
-export { default as XloginBox } from "./XloginBox/XloginBox";
-export { default as XTable } from "./Xtable/Xtable";
-export { default as XThead } from "./Xtable/Xthead";
-export { default as XTbody } from "./Xtable/Xtbody";
-export { default as XTfoot } from "./Xtable/Xtfoot";
-export { XTr, XTh, XTd, XTf } from "./Xtable/Xtrhdf";
+export * from "./Xbackground/Xbackground";
+export * from "./XdataMap/XdataMap";
+export * from "./XgetStart/XgetStart";
+export * from "./Xpanel/Xpanel";
+export * from "./XmenuList/XmenuList";
+export * from "./XloginBox/XloginBox";
+export * from "./Xtable/Xtable";
+export * from "./Xtable/Xthead";
+export * from "./Xtable/Xtbody";
+export * from "./Xtable/Xtfoot";
+export * from "./Xtable/Xtrhdf";
